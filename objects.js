96780fb5286d30d34b8ead65ce4a593c92a9667e
gcho91 <gcho91@gmail.com>
2017-08-28 23:05:02 -0500

    Solved for problem sets, should be reviewed over weekend

--- objects.js
+++ objects.js
@@ -5,43 +5,54 @@
 var user = {
   name: "Gloria",
   age: 25,
-  password: coffee123
+  password: "coffee123"
 };
 
 // Add an email and username, as well as a method called alertUser that alerts the users name, to your user object using dot notation.
 
 // Code Here
+user.email = "gcho91@gmail.com";
+user.username = "gcho91";
 
-
+user.alertUser = function () {
+  alert(user.name);
+}
 
 // Now loop over your user object and alert each of the keys.
 
 // Code Here
-
+for (var key in user) {
+  alert(key);
+}
 
 
 // Now loop over your user object and alert each of the values for each key.
 
 // Code Here
-
-
+for (var key in user) {
+  alert(user[key]);
+}
 
 // Remove the username from your user object
 
 // Code Here
-
+delete user.username;
 
 
 // Create an object called creditCard that has number, cvc, type, and limit properties and assign them appropriate values.
 
 // Code Here
 
-
-
+var creditCard = {
+  number: 1234567,
+  cvc: 145,
+  type: Visa,
+  limit: 2000
+};
 // Create a variable called alternating. Set it equal to the string bonusCategories.
 
 // Code Here
-
+var alternating = "bonusCategories";
 
 
 /* Using bracket notation, add a bonusCategories key to your creditCard object using 'alternating'.
@@ -56,12 +67,27 @@ var user = {
     */
 
 // Code Here
+creditCard[alternating] = [{
+    Department Stores: '5%',
+},{
+    grocery stores: '4%'
+},{
+    gas stations: '6%'
+}];
+
+creditCard.bank = {"bank name": "Chase", "address": "500 S Ervay St"};
+
+
 
 
 
 // Write a function that will take your creditCard object and user object as arguments and combine them into one object. /* HINT: You will need a for..in loop */
 
 // Code Here
+
+for (var key in user) {
+  creditCard[key] = user[key];
+}
 
 
 /*Below you are given an object. Write a function that will loop over the allies property, remove LittleFinger, and insert Dragon Lady.
@@ -75,3 +101,10 @@ var obj = {
   };
 
   // Code Here
+
+  function jonSnowKnowNothing (objs) {
+      for (var key in objs) {
+        objs[key][2] = 'Dragon Lady';
+      }
+      objs.alive = 'More Than Ever';
+  }
